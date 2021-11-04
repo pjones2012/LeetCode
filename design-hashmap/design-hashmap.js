e7e4e38ba8a15b44985498ba931cb09d9dc6efe4
Portia Jones <portiamelissa90@gmail.com>
2021-11-04 12:53:22 -0500

    Time: 192 ms (88.41%), Space: 48.2 MB (71.37%) - LeetHub

---
+++ design-hashmap/design-hashmap.js
@@ -0,0 +1,40 @@
+
+var MyHashMap = function() {
+    
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function(key, value) {
+    this[key] = value;
+    //console.log(this);
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function(key) {
+    return this[key]=== undefined? -1:this[key];
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashMap.prototype.remove = function(key) {
+    if(this[key]!== undefined){
+        delete this[key];
+    }
+};
+
+/** 
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
